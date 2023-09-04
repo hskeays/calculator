5a98367a30ff5f11abe3bee2c0d5a5312ee4ce22
hskeays <hskeays@gmail.com>
2023-09-04 11:09:14 -0400

    Create calculator operator functions

---
+++ index.js
@@ -0,0 +1,23 @@
+const add = (var1, var2) => {
+    return var1 += var2
+}
+
+const subtract = (var1, var2) => {
+    return var1 -= var2
+}
+
+const multiply = (var1, var2) => {
+    return var1 *= var2
+}
+
+const divide = (var1, var2) => {
+    return var1 /= var2
+}
+
+let variable1
+let variable2
+let operator
+
+const operate = () => {
+    return operator
+}
